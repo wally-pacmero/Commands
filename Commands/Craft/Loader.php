1fea0787c58f753f46995201d634e39afbc5ed34
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2021-03-31 13:13:22 -0500

    Update Loader.php

--- Commands/Craft/Loader.php
+++ Commands/Craft/Loader.php
@@ -1,1 +1,21 @@
+<?php
+
+namespace ;
+
+use \
+use \;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as TE;
+
+class Loader extends PluginBase implements Listener {
+
+public function onEnable(){
+    $this->getLogger()->info(TE::GREEN ."Craft Enable");
+}
+
+public function onDisable(){
+    $this->getLogger()->info(TE::RED ."Craft Disable");
+}
+}
 
