aeb975de6c53a53746eb617bb00d9d0fb49d497d
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2021-03-31 13:08:55 -0500

    Update Loader.php

--- Commands/Announce/Loader.php
+++ Commands/Announce/Loader.php
@@ -1,1 +1,21 @@
+<?php
+
+namespace ;
+
+use \
+use \;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as TE;
+
+class Loader extends PluginBase implements Listener {
+
+public function onEnable(){
+    $this->getLogger()->info(TE::GREEN ."Announce Enable");
+}
+
+public function onDisable(){
+    $this->getLogger()->info(TE::RED ."Announce Disable");
+}
+}
 
