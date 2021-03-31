efce6a476ca9be47dfedf6233f875d60f3ba686c
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2021-03-31 13:14:12 -0500

    Create CraftCommand.php

---
+++ Commands/Craft/CraftCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Commands\CraftCommand;
+
+use pocketmine\block\Block;
+use pocketmine\block\BlockIds;
+use pocketmine\block\CraftingTable;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\inventory\CraftingGrid;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\ContainerOpenPacket;
+use pocketmine\network\mcpe\protocol\InventoryContentPacket;
+use pocketmine\network\mcpe\protocol\types\inventory\ItemStackWrapper;
+use pocketmine\network\mcpe\protocol\types\WindowTypes;
+use pocketmine\Player;
+
+class CraftCommand extends Command
+{
+
+    public function __construct()
+    {
+        parent::__construct("craft", "Send CraftInventory");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if(!($sender instanceof Player)) return;
+
+        $vector = new Vector3($sender->x, $sender->y - 2, $sender->z);
+
+        $blockNew = Block::get(BlockIds::CRAFTING_TABLE);
+
+        $sender->getLevel()->setBlock($vector, $blockNew);
+
+        $blockNew = $sender->getLevel()->getBlock($vector);
+
+        $blockNew->onActivate($sender->getInventory()->getItemInHand(), $sender);
+
+    }
+}
