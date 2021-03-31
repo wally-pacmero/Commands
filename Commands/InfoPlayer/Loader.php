da5fc321ac1db35f1ef67c18b386ad95f30e73fc
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2021-03-31 14:36:43 -0500

    Update Loader.php

--- Commands/InfoPlayer/Loader.php
+++ Commands/InfoPlayer/Loader.php
@@ -12,13 +12,10 @@ class Loader extends PluginBase implements Listener {
 
 public function onEnable(){
     $this->getLogger()->info(TE::GREEN ."InfoPlayer Enable");
+    $this->getServer()->getCommandMap()->register("InfoPlayerCommand", new InfoPlayerCommand());
 }
 
 public function onDisable(){
     $this->getLogger()->info(TE::RED ."InfoPlayer Disable");
-}
-public function __construct(Loader $core) {
-        $this->core = $core;
-        $this->registerCommand(new AnnounceCommand());
 }
 }
